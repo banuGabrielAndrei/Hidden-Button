5236de72399dd4fa1b89a338053889c9a66c685c
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2023-05-25 20:03:57 +0300

    hiddenButton

---
+++ index.js
@@ -0,0 +1,20 @@
+let winnerNumber = Math.floor(Math.random() * 3) + 1
+let buttons = document.getElementsByClassName("btn btn-outline-primary")
+
+function checkWinnerButton() {
+    for (let i = 0 ; i < buttons.length; ++i) {
+        buttons[i].addEventListener('click', function(e) {
+            if (buttons[i].id == winnerNumber) {
+                buttons[i].innerHTML = "WINNER!"
+                document.getElementById(i + 1).style.backgroundColor = 'green'
+            } else {
+                buttons[i].innerHTML = "TRY AGAIN !"
+                document.getElementById(i + 1).style.backgroundColor = 'red'
+            }
+        })
+    }
+}
+
+
+
+

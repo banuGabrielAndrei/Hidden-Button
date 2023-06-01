2454f05d1281ab7ef03ba717fa6d37d03cdfed87
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2023-06-01 23:46:49 +0300

    H Buttons

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-let winnerNumber = Math.floor(Math.random() * 3) + 1
-let buttons = document.getElementsByClassName("btn btn-outline-primary")
+let winnerNumber = Math.floor(Math.random() * 3) + 1;
+let buttons = document.getElementsByClassName("btns");
 
-function checkWinnerButton() {
-    for (let i = 0 ; i < buttons.length; ++i) {
-        buttons[i].addEventListener('click', function(e) {
-            if (buttons[i].id == winnerNumber) {
-                buttons[i].innerHTML = "WINNER!"
-                document.getElementById(i + 1).style.backgroundColor = 'green'
-            } else {
-                buttons[i].innerHTML = "TRY AGAIN !"
-                document.getElementById(i + 1).style.backgroundColor = 'red'
-            }
-        })
+function buttonsSetup() {
+    for (let i = 0; i < buttons.length; ++i) {
+        buttons[i].addEventListener('click', checkWinnerButton);
     }
 }
-checkWinnerButton()
+
+let nrOfBtns;
+let secondWinnerNumber;
+let newBtns = [];
+function generateButtons() {
+    removeButtons();
+    nrOfBtns = document.getElementById("nrOfButtons").value;
+    secondWinnerNumber = Math.floor(Math.random() * nrOfBtns) + 1;
+    for (let i = 0; i < nrOfBtns; ++i) {
+        newBtns[i] = document.createElement('button');
+        newBtns[i].id = "btn" + (i + 1);
+        newBtns[i].innerHTML = "??";
+        newBtns[i].className = "btns1";
+        document.getElementById("generatedButtons").appendChild(newBtns[i]);
+        newBtns[i].addEventListener('click', checkWinnerButton);
+    }
+}
+
+function removeButtons() {
+    for (let i = 0; i < newBtns.length; ++i) {
+        newBtns[i].remove();
+    }
+    newBtns = [];
+}
+
+function checkWinnerButton(e) {
+    if (e.target.id == winnerNumber) {
+        e.target.innerHTML = "Winner";
+        e.target.style.backgroundColor = "green";
+    } else if (e.target.id == 'btn' + secondWinnerNumber){
+        e.target.innerHTML = "Winner";
+        e.target.style.backgroundColor = "green";
+    } else {
+        e.target.innerHTML = "Loser";
+        e.target.style.backgroundColor = "red";
+    }
+}
+
+buttonsSetup();
+
+           
+
+
+
+
 
 
 
